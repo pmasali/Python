fa463722e6039aefba443761c039f7efabd1b6da
Prateek Masali <prateek.masali@outlook.com>
2019-03-15 18:50:31 -0400

    updates

--- Python Problem Solving/solution.py
+++ Python Problem Solving/solution.py
@@ -1,8 +1,28 @@
-# with open("/Users/prateekmasali/Desktop/data.txt", "r") as ins:
-#     array = []
-#     for line in ins:
-#         array.append(rstrip(line))
-    
+with open("/Users/prateekmasali/Desktop/data.txt", "r") as ins:
+    array = []
+    for line in ins:
+        line = line.strip()
+        array.append(line.split(','))
+
+final = []
+flag = []
+for i, record in enumerate(array):
+    if i not in flag:
+        for j, entry in enumerate(array):
+            if array[j][0] == record[0] and array[j][1] == record[1] and j != i:
+                buffer = array[j][0],array[j][1],array[j][2]+','+array[i][2]
+                final.append(buffer)
+                flag.append(j)
+
+print(final)
+
+
+    # print(array[0][0])
+
+
+
+
+
 # lines = [line.rstrip('\n') for line in open('/Users/prateekmasali/Desktop/data.txt')]
 # # print(lines)
 # array = []
@@ -12,12 +32,12 @@
 # for record in array:
 
 
-import pandas as pd
-from collections import namedtuple
+# import pandas as pd
+# from collections import namedtuple
 
-Item = namedtuple('Patient', 'AdminDate','Drug')
-items = []
+# Item = namedtuple('Patient', 'AdminDate','Drug')
+# items = []
 
-df = pd.read_csv('/Users/prateekmasali/Desktop/data.txt', sep=",", names=['Patient', 'AdminDate','Drug'])
+# df = pd.read_csv('/Users/prateekmasali/Desktop/data.txt', sep=",", names=['Patient', 'AdminDate','Drug'])
 
-print(df)
+# print(df)
