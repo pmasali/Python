787215a7489599edd5228abe3e152b6ccde8c9c0
Prateek Masali <prateek.masali@outlook.com>
2019-04-23 01:58:41 -0400

    MultiFileLoad

---
+++ Python Projects/MultiFileLoad/mapper_multifile.py
@@ -0,0 +1,41 @@
+import os
+
+inputdir = '/Users/prateekmasali/Desktop/Git/Python/Python Projects/MultiFileLoad/'
+outputdir = '/Users/prateekmasali/Desktop/Git/Python/Python Projects/MultiFileLoad/Output/'
+
+filelist = os.listdir(inputdir)
+records = []
+max = 0
+for i in filelist:
+    with open(inputdir+i,'r') as f:
+        next(f)
+        for line in f:
+            fline = f.readlines()
+            for record in fline:
+                record = record.strip()
+                records.append(record.split('\t'))
+records = sorted(records,key = lambda x:int(x[0]))
+print(records)
+for record in records:
+    if int(record[1]) > max:
+        max = int(record[1])
+print('highest score across all files:', max)
+recordSum = {}
+recordCount = {}
+for rec in records:
+    try:
+        recordSum[rec[0]] = recordSum[rec[0]]+int(rec[1])
+    except:
+        recordSum[rec[0]] = int(rec[1])
+    recordCount[rec[0]] = recordCount.get(rec[0],0) + 1
+
+print('Sum of all gamers:', recordSum)
+print('Count of all gamers:', recordCount)    
+
+recordAvg = {}
+
+for k,v in recordSum.items():
+    avg = round(recordSum[k] / recordCount[k],2)
+    recordAvg[k] = avg
+
+print('Avg of all gamers:', recordAvg)
